c81869181dae7e1f35e37ad8a9f8b6614035edff
Aaron Zhi-Ren Shey <aaron.z.shey@proton.me>
2024-07-25 10:21:20 -0700

    init

---
+++ components/Test.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+interface Props {
+  children?: ReactNode;
+  // any props that come into the component
+}
+export default function Test({ children }: Props) {
+  return (
+    <div style={{ backgroundColor: "red", height: "auto", width: "auto" }}>
+      <div>{children}</div>
+    </div>
+  );
+}
